652ee4d38153f68fdc5edb29717b42f139ce6f81
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:22:47 +0300

    Add project files.

---
+++ TestString/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace TestString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.Convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 11F);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(264, 124);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Calibri", 11F);
+            this.richTextBox2.Location = new System.Drawing.Point(12, 142);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(264, 128);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(201, 279);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(75, 25);
+            this.Convert.TabIndex = 2;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 312);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button Convert;
+    }
+}
+
---
+++ TestString/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TestString
+{
+    public partial class Form1 : Form
+    {
+        public static int length = 2;
+        public static String allChars = "ABCDEF0123456789";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            convert();
+        }
+
+        private void convert() {
+            TimeSpan time = new TimeSpan();
+            DateTime time1 = DateTime.Now;
+
+            richTextBox2.Text = fastConvertTo(richTextBox1.Text).Replace("-", "").Replace("\n", "");
+
+            time = DateTime.Now - time1;
+            Debug.WriteLine(time.TotalSeconds + " seconds");
+
+            ////////
+
+            time = new TimeSpan();
+            time1 = DateTime.Now;
+
+            richTextBox2.Text = fastConvertFrom(richTextBox2.Text);
+
+            time = DateTime.Now - time1;
+            Debug.WriteLine(time.TotalSeconds + " seconds");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static String fastConvertTo(String text) {
+            byte[] bytes = Encoding.Unicode.GetBytes(text);
+
+            var result = BitConverter.ToString(bytes);
+
+            return result;
+        }
+
+        public static String fastConvertFrom(String text)
+        {
+            text = text.Replace(" ", "").Replace("-", "").Replace("\n", "");
+            byte[] bytes = null;
+
+            bytes = Enumerable.Range(0, text.Length / 2).Select(x => System.Convert.ToByte(text.Substring(x * 2, 2), 16)).ToArray();
+
+            return Encoding.Unicode.GetString(bytes);
+        }
+
+        public static String convertTo(String text) {
+            String newtext = "";
+
+            byte[] textBytes = Encoding.Unicode.GetBytes(text);
+            for (int i = 0; i < textBytes.Length; i++) {
+                //GET INDEX
+                int remain = textBytes[i];
+                for (int b = 0; b < length; b++) {
+                    int divideBy = Int32.Parse(Math.Pow(allChars.Length, length - 1 - b).ToString());
+                    newtext += allChars[remain / divideBy];
+                    remain = remain % divideBy;
+                }
+            }
+
+            return newtext;
+        }
+
+        public static String convertFrom(String text)
+        {
+            List<int> indexList = new List<int>();
+
+            text = text.Replace(" ", "").Replace("\n", "");
+
+            byte[] byteArray = new byte[text.Length / length];
+            for (int i = 0; i < text.Length; i += 2)
+            {
+                String subString = text.Substring(i, 2);
+                int index = 0;
+                for (int b = 0; b < subString.Length; b++)
+                {
+                    int divideBy = Int32.Parse(Math.Pow(allChars.Length, length - 1 - b).ToString());
+                    index += allChars.IndexOf(subString[b]) * divideBy;
+                }
+                byteArray[i / length] = (byte)index;
+            }
+
+            return Encoding.Unicode.GetString(byteArray);
+        }
+    }
+}
